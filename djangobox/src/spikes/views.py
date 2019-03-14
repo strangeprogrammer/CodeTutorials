c89fc4fa863b1e4ffb333e2261e53ab586efd23d
Stephen Fedele <stephen.m.fedele@wmich.edu>
2019-03-14 18:03:00 -0400

    Created a spike for sending and receiving GET, POST, and cookie information from the browser.

--- djangobox/src/spikes/views.py
+++ djangobox/src/spikes/views.py
@@ -3,5 +3,15 @@ from django.shortcuts import render
 def uptest(request, *args, **kwargs):
 	return render(request, 'uptest.html', {})
 
-def editorpush(request, *args, **kwargs):
+def editorPush(request, *args, **kwargs):
 	return render(request, 'editorpush.html', {})
+
+def requestSpike(request, *args, **kwargs):
+	context = { 'method': request.method or 'N/A', 'last': 'N/A', 'cookies': request.COOKIES }
+	
+	if request.method == 'POST':
+		context['last'] = request.POST.get('key', default=None) or 'N/A'
+	else:
+		context['last'] = request.GET.get('key', default=None) or 'N/A'
+	
+	return render(request, 'requestSpike.html', context)
