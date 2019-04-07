99469b775e04324dc2bbc7bfc4865c8e3d056469
Daniel Darcy” <daniel.m.darcy@wmich.edu>
2019-04-07 19:19:10 -0400

    Need to commit for merge. No changes made.

--- djangobox/src/pages/urls.py
+++ djangobox/src/pages/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from pages.views import index	#DMD
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
