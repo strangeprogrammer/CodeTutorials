169c2ab77aabcae8f91c737363297ce1dd27212a
Stephen Fedele <stephen.m.fedele@wmich.edu>
2019-05-13 21:41:47 -0400

    Made preset code and preset STDIN elements optional for class 'CodeRunner'. Added static function 'CodeRunner.cleancode' to 'docker/codeClient.js', which basically makes sure that the preloaded code in the HTML document renders properly within the 'code' textarea. Added file 'docker/templates/CodeRunner.html' as a practically usable example template of how to create the HTML structure that class 'CodeRunner' looks for.

--- djangobox/src/docker/static/docker/codeClient.js
+++ djangobox/src/docker/static/docker/codeClient.js
@@ -1,10 +1,11 @@
 /* Class 'CodeRunner':
  * url:		The URL to which code should be forwarded to
  * region:	A CSS selector that should contain elements from which data is read/written by AJAX
+ * timeout:	How long the AJAX server request should wait before timing out
  * 
  * Note: The 'region' selector's corresponding HTML element should have children HTML elements with the following classes:
- * 'presetcode':	Should contain the preset code and be hidden
- * 'presetSTDIN':	Should contain the preset STDIN and be hidden
+ * 'presetcode':	OPTIONAL: Should contain the preset code (and be hidden)
+ * 'presetSTDIN':	OPTIONAL: Should contain the preset STDIN (and be hidden)
  * 'code':		From where program code is taken
  * 'STDIN':		From where standard input for the program is taken
  * 'language':		From where the language of the program code is taken
@@ -14,15 +15,13 @@
  * 'STDERR':		To where the standard error of the program is put
  * 'retval':		To where the return value of the program is put
  */
-function CodeRunner(url, region, timeout = 5000){
+function CodeRunner(url, region, timeout = 5000){//5 * 1000 milliseconds = 5 seconds
 	//Get global variables
 	this.url	= url;
 	this.region	= region;
-	this.timeout	= timeout; //5 * 1000 milliseconds = 5 seconds
+	this.timeout	= timeout;
 	
-	//Get relevant HTML elements
-	this.pcode	= document.querySelector(region + ' .presetcode');
-	this.pSTDIN	= document.querySelector(region + ' .presetSTDIN');
+	//Get necessary HTML elements
 	this.code	= document.querySelector(region + ' .code');
 	this.STDIN	= document.querySelector(region + ' .STDIN');
 	this.mode	= document.querySelector(region + ' .codelang');
@@ -31,6 +30,23 @@ function CodeRunner(url, region, timeout = 5000){
 	this.STDOUT	= document.querySelector(region + ' .STDOUT');
 	this.STDERR	= document.querySelector(region + ' .STDERR');
 	this.retval	= document.querySelector(region + ' .retval');
+	
+	//Get optional HTML elements
+	var x = document.querySelector(region + ' .presetcode');
+	this.pcode = "";
+	if(x !== null){
+		this.pcode = x.value;
+		x.hidden = 'hidden';
+	}
+	var y = document.querySelector(region + ' .presetSTDIN');
+	this.pSTDIN = "";
+	if(y !== null){
+		this.pSTDIN = y.value;
+		y.hidden = 'hidden';
+	}
+	
+	//Get csrf token for form submission
+	this.csrf	= document.querySelector(region + ' [name=csrfmiddlewaretoken]').value;
 	
 	//Perform setup
 	this.registerFormSend();
@@ -50,10 +66,7 @@ CodeRunner.prototype.sendForm = function(form, success, failure, timeout){
 	var xhr = new XMLHttpRequest();
 	xhr.open('POST', this.url, true);
 	
-	//Get csrf token when submitting form
-	var csrf = document.querySelector('[name=csrfmiddlewaretoken]');
-	xhr.setRequestHeader('X-CSRFToken', csrf.value);
-	
+	xhr.setRequestHeader('X-CSRFToken', this.csrf);
 	xhr.timeout = this.timeout;
 	
 	xhr.onload = function(){
@@ -63,7 +76,7 @@ CodeRunner.prototype.sendForm = function(form, success, failure, timeout){
 		//Bad response
 		}else{
 			failure();
-		}
+		};
 	};
 	
 	xhr.ontimeout = function(){
@@ -102,9 +115,52 @@ CodeRunner.prototype.registerReset = function(){
 };
 
 CodeRunner.prototype.preset = function(){
-	this.code.value		= this.pcode.value;
-	this.STDIN.value	= this.pSTDIN.value;
+	this.code.value		= CodeRunner.cleancode(this.pcode);
+	this.STDIN.value	= CodeRunner.cleancode(this.pSTDIN);
 	this.STDOUT.value	= '';
 	this.STDERR.value	= '';
 	this.retval.value	= '';
+};
+
+//Returns true if the line (string) contains only whitespace and false otherwise
+CodeRunner.iswhite = function(line){
+	if(line.trim().length === 0){
+		return true;
+	};
+	return false;
+};
+
+/* Argument:
+ * code: A snippet of code
+ * 
+ * Description:
+ * Removes excess indentation of the code block
+ * 
+ * Notes & Extras:
+ * Removes trailing whitespace and leading blank lines
+ * Ignores blank lines when performing the computation of how much indentation to remove, but still removes all acceptable indentation from them
+ * Please do not use mixed indenation (both spaces and tabs)
+ */
+CodeRunner.cleancode = function(code){
+	//Remove all trailing whitespace and split up the result into individual lines
+	var lines = code.trimEnd().split('\n');
+	
+	//Remove all leading lines that contain only whitespace
+	var start = 0;
+	for(; start < lines.length && CodeRunner.iswhite(lines[start]); start++);
+	lines = lines.slice(start, lines.length);
+	
+	//Find the minimum amount of whitespace that can be removed safely
+	var minimum = Infinity;
+	for(i in lines){
+		diff = lines[i].length - lines[i].trimStart().length;
+		if(diff < minimum && ! CodeRunner.iswhite(lines[i])){
+			minimum = diff;
+		};
+	};
+	
+	//Remove all the whitespace, concatenate the results, and return the answer
+	return lines.map(function(x){
+		return x.slice(minimum, x.length);
+	}).join('\n');
 };
