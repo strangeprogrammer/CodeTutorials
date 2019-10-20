7a733bcf3a3bf3248c3c3d8b40a6c4d8a723e1a3
Stephen Fedele <stephen.m.fedele@wmich.edu>
2019-10-19 20:26:04 -0400

    Updated 'install.sh'.

--- install.sh
+++ install.sh
@@ -32,54 +32,86 @@ case "$REPLY" in
 esac
 
 # Set up project configuration
+echo
+echo "Setting up project configuration..."
+set +e
+set +o pipefail
+rm ./.settings.sh &>/dev/null
 source ./devtools.sh # TODO: PUT INFORMATION IN README FILE ABOUT THIS PART
-sudo openproj
+set -e
+set -o pipefail
+
+# Change project permissions as appropriate
+echo
+echo "Updating project permissions..."
+openproj
 if $DEVELOPMENT && $DEPLOYMENT
 then
-	sudo devdeploy .
+	devdeploy .
 elif $DEVELOPMENT
 then
-	sudo developer.
+	developer.
 elif $DEPLOYMENT
 then
-	sudo deployment .
+	deployment .
 fi
 
 # Set up and activate virtual environment
+echo
+echo "Creating virtual environment for python..."
 virtualenv -p python3 ./djangobox/
-pushd ./djangobox/
+pushd ./djangobox/ &>/dev/null
 source ./bin/activate
-popd
+popd &>/dev/null
 
 # Install python packages
+echo
+echo "Installing Django and useful packages in virtual environment..."
 pip3 install Django==2.1.7 djangocodemirror==2.1.0 django-bootstrap3==11.1.0
 
 # Add relevant users to group 'docker'
+echo
 if $DEVELOPMENT
 then
+	echo "Adding '$DEV_USER' to group 'docker'..."
 	sudo usermod -aG docker $DEV_USER
 fi
 if $DEPLOYMENT
 then
+	echo "Adding '$SERVER_USER' to group 'docker'..."
 	sudo usermod -aG docker $SERVER_USER
 fi
 
-# Set up database, collect static files, and build Docker images
-pushd ./djangobox/src/
+# Set up database
+echo
+echo "Setting up database..."
+pushd ./djangobox/src/ &>/dev/null
 ./manage.py makemigrations
 ./manage.py migrate
+popd &>/dev/null
+
+# Collect static files
+echo
+echo "Collecting static files..."
+pushd ./djangobox/src/ &>/dev/null
 ./manage.py collectstatic <<<"yes"
-pushd ./docker/docker_wrapper/
+popd &>/dev/null
+
+# Build Docker images
+echo
+echo "Building docker images..."
+pushd ./djangobox/src/docker/docker_wrapper/ &>/dev/null
 sudo systemctl start containerd.service docker.service docker.socket
 sudo ./buildimages.sh # This script uses 'sudo' on its own, but this simplifies things a bit
 sudo systemctl stop containerd.service docker.service docker.socket
-popd
-popd
+popd &>/dev/null
 
 # Potentially, symlink the static directory for Apache
-pushd /var/www/html/
-ln -s -T $OLDPWD/djangobox/src/static/ ./static/
-popd
+echo
+echo "Symlinking static files directory for Apache..."
+pushd /var/www/html/ &>/dev/null
+sudo ln -s -T $OLDPWD/djangobox/src/static/ ./static
+popd &>/dev/null
 
 # Cleanup
 undevtools
